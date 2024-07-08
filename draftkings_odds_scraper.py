7967faa4aaa0405208d7a54e439f5d5b49339950
N3NU <44658521+N3NU@users.noreply.github.com>
2024-07-08 00:36:15 -0400

    Update draftkings_odds_scraper.py
    Version 2

--- draftkings_odds_scraper.py
+++ draftkings_odds_scraper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 from send_message import send_email
+import os
 
 
 def scrape_dk():
@@ -63,9 +64,12 @@ def scrape_dk():
         print(f"Failed to retrieve data: {response.status_code}")
         return None
     
-# Function to save data to a CSV file
-def save_data_to_csv(df, filename):
-    df.to_csv(filename, index=False)
+# Function to append data to a CSV file
+def append_data_to_csv(df, file_path):
+    if not os.path.isfile(file_path):
+        df.to_csv(file_path, index=False)
+    else:
+        df.to_csv(file_path, mode='a', header=False, index=False)
 
 # Function to load data from a CSV file
 def load_data_from_csv(filename):
@@ -76,35 +80,45 @@ def load_data_from_csv(filename):
         return pd.DataFrame(columns=['fighter_1', 'fighter_1_odds', 'fighter_2', 'fighter_2_odds', 'fighter_bout_id'])
     
 def main():
-    csv_filename = 'UFC_fight_odds.csv'
-    previous_df = load_data_from_csv(csv_filename)
+    # Get the current directory of the script
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+
+    # Define the path to the CSV file
+    csv_filename = os.path.join(current_directory, 'UFC_fight_odds.csv')
+
+    
     
     while True:
         print('running')
+
+        stored_fight_data_df = load_data_from_csv(csv_filename)
+
         scraped_data = scrape_dk()
 
         if scraped_data:
             # Create new dataframe using data scraped from DK website
-            current_df = pd.DataFrame(scraped_data, columns=['fighter_1', 'fighter_1_odds', 'fighter_2', 'fighter_2_odds', 'fighter_bout_id'])
+            newly_scraped_fight_data_df = pd.DataFrame(scraped_data, columns=['fighter_1', 'fighter_1_odds', 'fighter_2', 'fighter_2_odds', 'fighter_bout_id'])
 
             # Identify new fights by comparing fighter_bout_id against previous data
             new_fights = []
-            for bout_id in current_df['fighter_bout_id'].tolist():
-                if bout_id not in previous_df['fighter_bout_id'].tolist():
+            for bout_id in newly_scraped_fight_data_df['fighter_bout_id'].tolist():
+                if bout_id not in stored_fight_data_df['fighter_bout_id'].tolist():
                     new_fights.append(bout_id)
 
             if new_fights:
-                new_fights_df = current_df[current_df['fighter_bout_id'].isin(new_fights)]
-                body = f"New fights detected:\n\n\n{new_fights_df[['fighter_1', 'fighter_1_odds', 'fighter_2', 'fighter_2_odds']].to_string(index=False, header=False)}"
-                print(body)
-                # Save current data to CSV file
-                save_data_to_csv(current_df, csv_filename)
-                send_email(csv_filename, body, "New UFC Fight Odds")
+                # This part creates a boolean Series that indicates whether each value in the 'fighter_bout_id' column of the newly_scraped_fight_data_df DataFrame is present in the new_fights list.
+                # isin(new_fights) checks each 'fighter_bout_id' against the new_fights list and returns True for rows where the 'fighter_bout_id' is in the list and False otherwise.
+                new_fights_df = newly_scraped_fight_data_df[newly_scraped_fight_data_df['fighter_bout_id'].isin(new_fights)]
+                body = f"{new_fights_df[['fighter_1', 'fighter_1_odds', 'fighter_2', 'fighter_2_odds']].to_string(index=False, header=False)}"
 
-            # Update previous data
-            previous_df = current_df.copy()
+                # Append new fight data to CSV file
+                append_data_to_csv(new_fights_df, csv_filename)
+
+                # Send email update of new fight data (name of file, message body, and subject)
+                send_email(csv_filename, body, "New UFC Fight Odds")
 
         time.sleep(60)  # Wait for 60 seconds before scraping again
 
 if __name__ == "__main__":
     main()
+
