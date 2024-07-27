34f6ee5d4d0d1543bbb699b6675b95199c59b0f2
n3nu <stevencaraballo27@gmail.com>
2024-07-27 01:18:41 -0400

    Updated the script to account for instances where the DK site has no fighters and odd number of fighters listed.

--- draftkings_odds_scraper.py
+++ draftkings_odds_scraper.py
@@ -4,67 +4,94 @@ import pandas as pd
 import time
 from send_message import send_email
 import os
-
+from datetime import datetime
 
 def scrape_dk():
     # URL of the DraftKings UFC odds page
     url = "https://sportsbook.draftkings.com/leagues/mma/ufc"
 
-    # Fetch the HTML content from the URL
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        # Store response content
-        html_content = response.content
-
-        # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(html_content, 'html.parser')
-
-        # Find the "root" id in the HTML content
-        root_id = soup.find(id="root")
-
-        # Make list of all the fighter names found in the HTML content within the "root" id
-        all_fighters_list = [fighter.text.strip() for fighter in root_id.find_all("div", class_="event-cell__name-text")]
-        
-        # Make a list of the first fighters in the bout using the list of all fighters
-        fighter_1 = [all_fighters_list[i] for i in range(0, len(all_fighters_list), 2)]
-
-        # Make a list of the second fighters (opponents) in the bout using the list of all fighters
-        fighter_2 = [all_fighters_list[i + 1] for i in range(0, len(all_fighters_list), 2)]
-
-        # Make a list of all the fighter moneyline odds
-        all_fighters_odds_list = [fighter_odds.text.strip() for fighter_odds in root_id.find_all("span", class_="sportsbook-odds american no-margin default-color")]
-
-        # Make a list of the odds for the first fighters
-        fighter_1_odds = [all_fighters_odds_list[i] for i in range(0, len(all_fighters_list), 2)]
-
-        # Make a list of the odds for the second fighters (opponents)
-        fighter_2_odds = [all_fighters_odds_list[i + 1] for i in range(0, len(all_fighters_list), 2)]
-
-        # Get all the HTML elements with "a"
-        all_a_tags = root_id.find_all("a")
-
-        # Pull the bout id from the href found in the "a" element
-        bout_ids = []
-        for a_tag in all_a_tags:
-            href = a_tag.get('href')
-            if href and '/event/' in href:
-                bout_id = href.split('/')[-1]
-                try:
-                    bout_ids.append(int(bout_id))
-                except:
-                    None
-        # Make a list of the bout ids 
-        fighter_bout_id = [bout_ids[i] for i in range(0, len(all_fighters_list), 2)]
-
-        # Sort the column data to make ready to return
-        data = list(zip(fighter_1, fighter_1_odds, fighter_2, fighter_2_odds, fighter_bout_id))
-
-        return data
+    max_retries = 5
     
-    else:
-        print(f"Failed to retrieve data: {response.status_code}")
-        return None
+    retry_delay = 60 # delay in seconds before retrying
+    
+    for attempt in range(max_retries):
+            
+        try:
+
+            # Fetch the HTML content from the URL
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                # Store response content
+                html_content = response.content
+
+                # Parse the HTML content using BeautifulSoup
+                soup = BeautifulSoup(html_content, 'html.parser')
+
+                # Find the "root" id in the HTML content
+                root_id = soup.find(id="root")
+
+                # Make list of all the fighter names found in the HTML content within the "root" id
+                all_fighters_list = []
+                # Make a list of all the fighter moneyline odds
+                all_fighters_odds_list = []
+                # Pull the bout id from the href found in the "a" element
+                bout_ids = []
+
+                # Looping through list of html elements that start with "tr"
+                for i in root_id.find_all("tr"):
+
+                    # Checking if fighter name and fighter odds exist. This must be done as the website occasionally omits odds and broke the old script because of that
+                    if "event-cell__name-text" in str(i) and "sportsbook-odds american no-margin default-color" in str(i):
+
+                        # Append fighter name to list
+                        all_fighters_list.append(i.find('div', class_='event-cell__name-text').text.strip())
+
+                        # Append fighter odds to list
+                        all_fighters_odds_list.append(i.find('span', class_='sportsbook-odds american no-margin default-color').text.strip())
+
+                        # Append fighter bout id to list
+                        bout_ids.append(int(i.find('a').get('href').split('/')[-1]))
+
+                if all_fighters_odds_list and all_fighters_odds_list % 2 == 0:
+                
+                    # Make a list of the first fighters in the bout using the list of all fighters
+                    fighter_1 = [all_fighters_list[i] for i in range(0, len(all_fighters_list), 2)]
+
+                    # Make a list of the second fighters (opponents) in the bout using the list of all fighters
+                    fighter_2 = [all_fighters_list[i + 1] for i in range(0, len(all_fighters_list), 2)]
+
+                    # Make a list of the odds for the first fighters
+                    fighter_1_odds = [all_fighters_odds_list[i] for i in range(0, len(all_fighters_list), 2)]
+
+                    # Make a list of the odds for the second fighters (opponents)
+                    fighter_2_odds = [all_fighters_odds_list[i + 1] for i in range(0, len(all_fighters_list), 2)]
+
+                    # Make a list of the bout ids 
+                    fighter_bout_id = [bout_ids[i] for i in range(0, len(all_fighters_list), 2)]
+
+                    # Sort the column data to make ready to return
+                    data = list(zip(fighter_1, fighter_1_odds, fighter_2, fighter_2_odds, fighter_bout_id))
+
+                    return data
+                
+                else:
+
+                    return False
+            
+            else:
+                current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                print(f"[{current_time}] Failed to retrieve data: {response.status_code}")
+                return None
+            
+        except requests.RequestException as e:
+            current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(f"[{current_time}] Request failed: {e}. Retrying in {retry_delay} seconds...")
+            time.sleep(retry_delay)
+
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(f"[{current_time}] Max retries reached. Failed to retrieve data.")
+    return None
     
 # Function to append data to a CSV file
 def append_data_to_csv(df, file_path):
@@ -91,7 +118,8 @@ def main():
     
     
     while True:
-        print('running')
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f'[{current_time}] running')
 
         stored_fight_data_df = load_data_from_csv(csv_filename)
 
@@ -119,7 +147,7 @@ def main():
                 # Send email update of new fight data (name of file, message body, and subject)
                 send_email(csv_filename, body, "New UFC Fight Odds")
 
-        time.sleep(60)  # Wait for 60 seconds before scraping again
+        time.sleep(10)  # Wait for 60 seconds before scraping again
 
 if __name__ == "__main__":
     main()
