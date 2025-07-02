7a87622e710420b96dc55a2e10f7fafce12a78bc
Ray <raypeaslee@gmail.com>
2025-07-02 11:28:44 -0400

    Update new_odds_scraper.py
    changes the updated_at column to a timestamp
    
    saves the DataFrame to odds.csv file

--- new_odds_scraper.py
+++ new_odds_scraper.py
@@ -14,19 +14,27 @@ total_bouts = data['bout_number'].max() # total number of bouts for this event
 data = data.sort_values(by='bout_number', ascending=False)
 
 # add new columns fighter_1_odds , fighter_2_odds and updated_at and add a default value for each
-data['updated_at'] = pd.Timestamp.now().strftime('%b-%d-%Y %I:%M:%p')
+data['updated_at'] = pd.Timestamp.now()
 data['fighter_1_odds'] = -100
 data['fighter_2_odds'] = +100
 
 # Define formatters to apply to the columns when printing
 formatters = {
     'fighter_1_odds': '{:+g}'.format,
-    'fighter_2_odds': '{:+g}'.format
+    'fighter_2_odds': '{:+g}'.format,
+    'updated_at': '{:%b-%d-%Y %I:%M:%p}'.format
 }
 
 data.sort_index(axis=1, inplace=True)  # Sort columns alphabetically
 
 print(data.head(total_bouts).to_string(index=False, formatters=formatters))
 
-#print(data.info())
+print(data.info())
 #print(data.describe())
+
+# Save the updated DataFrame to a CSV file named 'odds.csv'
+output_csv_path = os.path.join(script_dir, "odds.csv")
+data.to_csv(output_csv_path, index=False)
+
+# Print the path where the CSV file is saved
+print(f"Updated odds saved to {output_csv_path}")
