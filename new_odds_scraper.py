736079aa4064d5655f18893d7a1ff45a5664e7fc
Ray <raypeaslee@gmail.com>
2025-07-02 09:49:24 -0400

    Update new_odds_scraper.py
    loads the correct csv file

--- new_odds_scraper.py
+++ new_odds_scraper.py
@@ -1,1 +1,14 @@
-#placeholder
+import pandas as pd
+import os
+
+# Get the directory where the current script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the full path to the CSV file
+csv_path = os.path.join(script_dir, "next_event", "next_event", "next_event.csv")
+
+data = pd.read_csv(csv_path)
+print(data.head())
+# Display the first few rows of the DataFrame
+print(data.info())
+print(data.describe())
